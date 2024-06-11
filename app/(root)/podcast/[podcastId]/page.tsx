9e6f891121fc1ed4bff98a671bdc5b55016a3aeb
vvu060 <vvu060@gmail.com>
2024-06-11 19:10:20 +0530

    add headers

--- app/(root)/podcast/[podcastId]/page.tsx
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const PodcastDetails = ({ params }: { params: { podcastId: string } }) => {
-  return <p className='text-white-1'>PodcastDetails for {params.podcastId}</p>;
+  return (
+    <p className='text-white-1'>
+      <h1 className='text-20 font-bold text-white-1'>
+        PodcastDetails for {params.podcastId}
+      </h1>
+    </p>
+  );
 };
 
 export default PodcastDetails;
