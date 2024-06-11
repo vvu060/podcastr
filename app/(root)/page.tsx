d8f0813ef4407fd5bf436be60bd0ca668165c5e4
vvu060 <vvu060@gmail.com>
2024-06-11 19:15:52 +0530

    finish podcast card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,15 +7,17 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending podcasts</h1>
-        {podcastData.map((podcast) => (
-          <PodcastCard
-            key={podcast.id}
-            imgURL={podcast.imgURL}
-            title={podcast.title}
-            description={podcast.description}
-            podcastId={podcast.id}
-          />
-        ))}
+        <div className='podcast_grid'>
+          {podcastData.map((podcast) => (
+            <PodcastCard
+              key={podcast.id}
+              imgURL={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
