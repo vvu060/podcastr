3d6aabe05368f12d301365dabc7c38f6acc48c24
vvu060 <vvu060@gmail.com>
2024-06-12 19:45:22 +0530

    add webhook for convex & clerk sync

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://humane-sculpin-86.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+
+export default authConfig;
