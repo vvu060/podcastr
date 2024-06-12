5b53b42073e9b86cac1ae250d64a1e6213ca37f7
vvu060 <vvu060@gmail.com>
2024-06-12 20:08:23 +0530

    change sign-in appearence

--- app/provider/ConvexClerkProvider.tsx
+++ app/provider/ConvexClerkProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ConvexReactClient } from 'convex/react';
 import { ReactNode } from 'react';
@@ -12,6 +12,19 @@ const convex = new ConvexReactClient(
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
   <ClerkProvider
     publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    appearance={{
+      layout: {
+        socialButtonsVariant: 'iconButton',
+        logoImageUrl: '/icons/auth-logo.svg',
+      },
+      variables: {
+        colorBackground: '#15171C',
+        colorPrimary: '',
+        colorText: 'white',
+        colorInputBackground: '#1b1f29',
+        colorInputText: 'white',
+      },
+    }}
   >
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
