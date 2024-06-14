3593d23f227d1f28a6262587e0b91f05f363a29f
vvu060 <vvu060@gmail.com>
2024-06-14 08:58:27 +0530

    create comps for AI thumbnail and voice

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,16 @@
+import { GeneratePodcastProps } from '@/types';
+import React from 'react';
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  audio,
+  setAudio,
+  setAudioDuration,
+  setVoicePrompt,
+  voicePrompt,
+  voiceType,
+}: GeneratePodcastProps) => {
+  return <div>GeneratePodcast</div>;
+};
+
+export default GeneratePodcast;
